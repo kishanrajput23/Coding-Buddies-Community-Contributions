8f9a505b5dd0d85441c46bc22757d578f8a68a73
Jaswanth Teja <sunnyteja201@gmail.com>
2022-10-09 07:09:30 +0530

    Create MultiplyNum.py
    This Script Multiply the 2 Numbers.

---
+++ MultiplyNumbers/MultiplyNum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Input Of Numbers from The User
+n1 = input('Enter first number: ')
+n2 = input('Enter second number: ')
+
+# Multiply The Given 2 Inputs (n1,n2) by the user
+product = float(n1) * float(n2)
+
+# Lets Print The product of the n1,n2
+print('The Product of 2 Numbers is {0}'.format(product))
