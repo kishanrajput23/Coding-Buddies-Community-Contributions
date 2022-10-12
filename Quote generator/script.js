b48d381d6df087107655236fcd1feb0acb08a821
Shasg121j <115630625+Shasg121j@users.noreply.github.com>
2022-10-12 19:33:14 +0530

    Quotest

---
+++ Quote generator/script.js
@@ -0,0 +1,20 @@
+const quote = document.querySelector
+("#quote");
+const author = document.querySelector
+("#author");
+const btn = document.querySelector
+("#btn");
+
+btn.addEventListener("click", getQuote);
+
+function getQuote(){
+    fetch("http://api.quotable.io/random")
+    .then(res => res.json())
+    .then(data => {
+        quote.innerHTML=`"${data.content}"`;
+        author.innerHTML=data.author;
+
+
+    })
+}
+getQuote();
