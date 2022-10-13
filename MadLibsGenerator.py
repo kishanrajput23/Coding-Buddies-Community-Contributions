3208c81e0787a1acaf006f08cbeadbae9ece7320
satyasharma01 <115723069+satyasharma01@users.noreply.github.com>
2022-10-13 22:03:51 +0530

    Create MadLibsGenerator.py
    This Python beginner project is a good start for beginners as it makes use of strings, variables, and concatenation. The Mad Libs Generator manipulates input data, which could be anything: an adjective, a pronoun, or verb. After taking in the input, the program takes the data and arranges it to build a story. This is a very cool Python project to try out if you’re new to coding.

---
+++ MadLibsGenerator.py
@@ -0,0 +1,35 @@
+loop = 1
+
+while (loop < 10):
+
+noun = input("Choose a noun: ")
+
+p_noun = input("Choose a plural noun: ")
+
+noun2 = input("Choose a noun: ")
+
+place = input("Name a place: ")
+
+adjective = input("Choose an adjective (Describing word): ")
+
+noun3 = input("Choose a noun: ")
+
+print ("------------------------------------------")
+
+print ("Be kind to your",noun,"- footed", p_noun)
+
+print ("For a duck may be somebody's", noun2,",")
+
+print ("Be kind to your",p_noun,"in",place)
+
+print ("Where the weather is always",adjective,".")
+
+print ()
+
+print ("You may think that is this the",noun3,",")
+
+print ("Well it is.")
+
+print ("------------------------------------------")
+
+loop = loop + 1
