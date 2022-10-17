e89771bba13f30a5e07395d3572ffe7b0c23cceb
Ahmed Misbahuddin <72156723+Misba9@users.noreply.github.com>
2022-10-17 15:57:53 +0530

    Modified BMI Calculator
    update the BMI calculator

--- BMI-Calculator/script.js
+++ BMI-Calculator/script.js
@@ -5,3 +5,11 @@
            var total = bmi.toFixed(2);
            document.getElementById('result').innerHTML = "Your BMI is "+ total
          }
+ window.onload = function () {
+            var images = ['BMI.jpg', 'BMI2.jpg', 'BMI3.jpg','BMI3.jpg', 'BMI5.jpg'];
+            var image = images[Math.floor(Math.random() * images.length)];
+            document.getElementsByTagName('body')[0].style.backgroundImage = "url('" + image + "')";
+            document.getElementsByTagName('body')[0].style.backgroundRepeat = "no-repeat";
+            document.getElementsByTagName('body')[0].style.backgroundSize = "cover";
+        }
+		alert("This page shows random background images");
